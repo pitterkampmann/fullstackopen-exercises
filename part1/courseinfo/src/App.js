d75550854a6879f8acc925d88ca64dffc7a85d56
Pitter <54911464+pitterkampmann@users.noreply.github.com>
2022-01-28 21:35:34 -0300

    Exercise: course information step3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,26 +2,28 @@ import React from "react";
 
 const App = () => {
 	const course = "Half Stack application development";
-	const part1 = "Fundamentals of React";
-	const exercises1 = 10;
-	const part2 = "Using props to pass data";
-	const exercises2 = 7;
-	const part3 = "State of a component";
-	const exercises3 = 14;
+	const part1 = {
+		name: "Fundamentals of React",
+		exercises: 10,
+	};
+	const part2 = {
+		name: "Using props to pass data",
+		exercises: 7,
+	};
+	const part3 = {
+		name: "State of a component",
+		exercises: 14,
+	};
 
 	return (
 		<div>
 			<Header course={course} />
-			<Content
-				part1={part1}
-				part2={part2}
-				part3={part3}
-				exercises1={exercises1}
-				exercises2={exercises2}
-				exercises3={exercises3}
-			/>
+			<Content part1={part1} part2={part2} part3={part3} />
 			<Total
-				text={"Number of exercises " + Number(exercises1 + exercises2 + exercises3)}
+				text={
+					"Number of exercises " +
+					Number(part1.exercises + part2.exercises + part3.exercises)
+				}
 			/>
 		</div>
 	);
@@ -33,19 +35,12 @@ export const Header = ({ course }) => {
 	return <h1>{course}</h1>;
 };
 
-const Content = ({
-	exercises1,
-	exercises2,
-	exercises3,
-	part1,
-	part2,
-	part3,
-}) => {
+const Content = ({ part1, part2, part3 }) => {
 	return (
 		<div>
-			<Part part={part1} exercise={exercises1} />
-			<Part part={part2} exercise={exercises2} />
-			<Part part={part3} exercise={exercises3} />
+			<Part part={part1.name} exercise={part1.exercise} />
+			<Part part={part2.name} exercise={part2.exercise} />
+			<Part part={part3.name} exercise={part3.exercise} />
 		</div>
 	);
 };
