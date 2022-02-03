7e917276b97499783a7861ecd3fd999e9874978a
Pitter <54911464+pitterkampmann@users.noreply.github.com>
2022-02-03 15:23:33 -0300

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 
 const App = () => {
 	const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", number: "123456789" },
+		{ name: "Arto Hellas", number: "040-123456", id: 1 },
+		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
+		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
 	]);
+
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
+	const [search, setSearch] = useState("");
 
 	const templateFn = (newName) => `${newName} is already added to phonebook`;
 
@@ -15,6 +20,10 @@ const App = () => {
 
 	const handleNumberChange = (event) => {
 		setNewNumber(event.target.value);
+	};
+
+	const handleSearchChange = (event) => {
+		setSearch(event.target.value);
 	};
 
 	const handleSubmit = (event) => {
@@ -36,7 +45,11 @@ const App = () => {
 		}
 	};
 
-	const list = persons.map((p) => {
+	const filtered = persons.filter((p) => {
+		return p.name.toLowerCase().includes(search.toLowerCase());
+	});
+
+	const list = filtered.map((p) => {
 		return (
 			<li key={p.name}>
 				{p.name} {p.number}
@@ -47,6 +60,11 @@ const App = () => {
 	return (
 		<div>
 			<h2>Phonebook</h2>
+			<div>
+				filter shown with:{" "}
+				<input value={search} onChange={handleSearchChange} />
+			</div>
+			<h2>Add New</h2>
 			<form onSubmit={handleSubmit}>
 				<div>
 					name: <input value={newName} onChange={handleNameChange} />
