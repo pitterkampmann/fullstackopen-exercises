53cd319f806adf918e0e315e74090dff1286136b
Pitter <54911464+pitterkampmann@users.noreply.github.com>
2022-02-08 15:58:56 -0300

    3.8*: Phonebook backend step8

--- part3/phonebook.api/index.js
+++ part3/phonebook.api/index.js
@@ -4,7 +4,20 @@ app.use(express.json());
 
 var morgan = require("morgan");
 
-app.use(morgan("tiny"));
+app.use(
+	morgan(function (tokens, req, res) {
+		return [
+			tokens.method(req, res),
+			tokens.url(req, res),
+			tokens.status(req, res),
+			tokens.res(req, res, "content-length"),
+			"-",
+			tokens["response-time"](req, res),
+			"ms",
+			JSON.stringify(req.body),
+		].join(" ");
+	})
+);
 
 let persons = [
 	{
